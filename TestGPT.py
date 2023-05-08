1aff1471ba10cef08ce8b709a9bb85bc8c8ffd73
ozBaget <oz052777@gmail.com>
2023-05-08 12:58:36 +0300

    SplitPage change, I dont remember extactly

---
+++ TestGPT.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+
+    return "Success"
+
+
+start_coord = (0, 100)  # Starting coordinate for the search
+
+print(crop_png_middle("C:\\Users\\izeik\\Pictures\\Mix\\question_12_prefix.png", start_coord))
